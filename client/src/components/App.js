e4d85fad7ad4f1aab76ae74b01ad04a9770b34ea
yannis <yannis.ayouaz@gmail.com>
2022-04-17 13:58:30 +0200

    commit

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,70 +1,115 @@
 import React, { Component } from "react";
-import SimpleStorageContract from "../contracts/SimpleStorage.json";
-import getWeb3 from "../getWeb3";
-
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import Card from 'react-bootstrap/Card';
+import ListGroup from 'react-bootstrap/ListGroup';
+import Table from 'react-bootstrap/Table';
+import Whitelist from "./contracts/Whitelist.json";
+import getWeb3 from "./getWeb3";
 import "../styles/App.css";
 
 class App extends Component {
-  state = { storageValue: 0, web3: null, accounts: null, contract: null };
+  state = { web3: null, accounts: null, contract: null, whitelist: null };
 
-  componentDidMount = async () => { 
+  componentWillMount = async () => {
     try {
-      // Get network provider and web3 instance.
+      // Récupérer le provider web3
       const web3 = await getWeb3();
-
-      // Use web3 to get the user's accounts.
+  
+      // Utiliser web3 pour récupérer les comptes de l’utilisateur (MetaMask dans notre cas) 
       const accounts = await web3.eth.getAccounts();
 
-      // Get the contract instance.
-      const networkId = await web3.eth.net.getId();
-      const deployedNetwork = SimpleStorageContract.networks[networkId];
+      // Récupérer l’instance du smart contract “Whitelist” avec web3 et les informations du déploiement du fichier (client/src/contracts/Whitelist.json)
+      const deployedNetwork = Whitelist.networks[3];
+  
       const instance = new web3.eth.Contract(
-        SimpleStorageContract.abi,
-        deployedNetwork && deployedNetwork.address,
+        Whitelist.abi,
+        "0x968E3E2c2250eC61B78846f400E0EC07C67cB424",
       );
 
       // Set web3, accounts, and contract to the state, and then proceed with an
       // example of interacting with the contract's methods.
-      this.setState({ web3, accounts, contract: instance }, this.runExample);
+      this.setState({ web3, accounts, contract: instance }, this.runInit);
     } catch (error) {
       // Catch any errors for any of the above operations.
       alert(
-        `Failed to load web3, accounts, or contract. Check console for details.`,
+        `Non-Ethereum browser detected. Can you please try to install MetaMask before starting.`,
       );
       console.error(error);
     }
   };
 
-  runExample = async () => {
+  runInit = async() => {
     const { accounts, contract } = this.state;
+  
+    // récupérer la liste des comptes autorisés
+    const whitelist = await contract.methods.getAddresses().call();
+    // Mettre à jour le state 
+    this.setState({ whitelist: whitelist });
+  }; 
 
-    // Stores a given value, 5 by default.
-    await contract.methods.set(5).send({ from: accounts[0] });
-
-    // Get the value from the contract to prove it worked.
-    const response = await contract.methods.get().call();
-
-    // Update state with the result.
-    this.setState({ storageValue: response });
-  };
+  whitelist = async() => {
+    const { accounts, contract } = this.state;
+    const address = this.address.value;
+    
+    // Interaction avec le smart contract pour ajouter un compte 
+    await contract.methods.whitelist(address).send({from: accounts[0]});
+    // Récupérer la liste des comptes autorisés
+    this.runInit();
+  }
+ 
 
   render() {
+    const { whitelist } = this.state;
     if (!this.state.web3) {
       return <div>Loading Web3, accounts, and contract...</div>;
     }
     return (
       <div className="App">
-        <h1>Good to Go!</h1>
-        <p>Your Truffle Box is installed and ready.</p>
-        <h2>Smart Contract Example</h2>
-        <p>
-          If your contracts compiled and migrated successfully, below will show
-          a stored value of 5 (by default).
-        </p>
-        <p>
-          Try changing the value stored on <strong>line 42</strong> of App.js.
-        </p>
-        <div>The stored value is: {this.state.storageValue}</div>
+        <div>
+            <h2 className="text-center">Système d'une liste blanche</h2>
+            <hr></hr>
+            <br></br>
+        </div>
+        <div style={{display: 'flex', justifyContent: 'center'}}>
+          <Card style={{ width: '50rem' }}>
+            <Card.Header><strong>Liste des comptes autorisés</strong></Card.Header>
+            <Card.Body>
+              <ListGroup variant="flush">
+                <ListGroup.Item>
+                  <Table striped bordered hover>
+                    <thead>
+                      <tr>
+                        <th>@</th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                      {whitelist !== null && 
+                        whitelist.map((a) => <tr><td>{a}</td></tr>)
+                      }
+                    </tbody>
+                  </Table>
+                </ListGroup.Item>
+              </ListGroup>
+            </Card.Body>
+          </Card>
+        </div>
+        <br></br>
+        <div style={{display: 'flex', justifyContent: 'center'}}>
+          <Card style={{ width: '50rem' }}>
+            <Card.Header><strong>Autoriser un nouveau compte</strong></Card.Header>
+            <Card.Body>
+              <Form.Group controlId="formAddress">
+                <Form.Control type="text" id="address"
+                ref={(input) => { this.address = input }}
+                />
+              </Form.Group>
+              <Button onClick={ this.whitelist } variant="dark" > Autoriser </Button>
+            </Card.Body>
+          </Card>
+          </div>
+        <br></br>
       </div>
     );
   }
